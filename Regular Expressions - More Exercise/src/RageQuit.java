3c1dd82f6d3ed14835bc305de773ae379c877fb4
StanchevaYoana <yoana.radoslavova@gmail.com>
2019-08-01 14:38:00 +0300

    Add tasks .

---
+++ Regular Expressions - More Exercise/src/RageQuit.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RageQuit {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine().toUpperCase();
+        StringBuilder output = new StringBuilder();
+
+        Pattern patternDigit = Pattern.compile("\\d+");
+        Matcher matcherDigit = patternDigit.matcher(input);
+        Pattern patternSymbol = Pattern.compile("[^\\d]+");
+        Matcher matcherSymbol = patternSymbol.matcher(input);
+
+        while (matcherDigit.find() && matcherSymbol.find()) {
+            String sequence = matcherSymbol.group();
+            int repeat = Integer.parseInt(matcherDigit.group());
+            output.append(String.valueOf(sequence).repeat(Math.max(0, repeat)));
+        }
+
+        Set<String> uniqueSet = new HashSet<>();
+        for (int i = 0; i < output.length(); i++) {
+            uniqueSet.add(String.valueOf(output.charAt(i)));
+        }
+        System.out.printf("Unique symbols used: %d\n", uniqueSet.size());
+        System.out.println(output);
+    }
+}
---
+++ Regular Expressions - More Exercise/src/WinningTicket.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class WinningTicket {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        String[] collection = input.split("[, ]+");
+
+        for (String s : collection) {
+
+            if (s.length() == 20) {
+                String left = s.substring(0, 10);
+                String right = s.substring(10, 20);
+                String leftMach = "l";
+                String rightMach = "r";
+
+                Pattern pattern = Pattern.compile("[\\\\@]{6,}|[\\\\$]{6,}|[\\\\#]{6,}|[\\\\^]{6,}");
+                Matcher lm = pattern.matcher(left);
+                Matcher rm = pattern.matcher(right);
+                if (lm.find()) {
+                    leftMach = lm.group();
+                }
+                if (rm.find()) {
+                    rightMach = rm.group();
+                }
+
+                if (leftMach.substring(0, 1).equals(rightMach.substring(0, 1))) {
+                    int matchCount = Math.min(leftMach.length(), rightMach.length());
+
+                    if (matchCount == 10) {
+                        System.out.printf("ticket \"%s\" - %d%s Jackpot!%n", s, matchCount, leftMach.substring(0, 1));
+                    } else {
+                        System.out.printf("ticket \"%s\" - %d%s%n", s, matchCount, leftMach.substring(0, 1));
+                    }
+
+                } else {
+                    System.out.printf("ticket \"%s\" - no match%n", s);
+                }
+
+            } else {
+                System.out.println("invalid ticket");
+            }
+        }
+    }
+}
